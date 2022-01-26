20182457542226535d2596102ba7a4e5c5205a2b
Richard Rogers <treyrogers073@gmail.com>
2022-01-26 11:49:24 -0700

    added middleware, instantiated the PORT

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
+// imported packages
 const mysql = require('mysql2');
-const app = require('express');
-const sequielize = require('sequelize');
+const express = require('express');
+const sequelize = require('./config/connection');
 const zipcodes = require('zipcodes')
 const session = require('session');
 const expSession = require('express-session');
+const routes = require('./controllers');
+const path = require('path');
 require('dotenv').config();
+
+//overhead express setup with instantiation and port number
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+//for use of front end when thats defined
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(routes);
+
+sequelize.sync({force:false}).then(() => {
+    app.listen(PORT, () => {
+        console.log('listening');
+    })
+});
