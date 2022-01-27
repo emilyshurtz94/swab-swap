51e1cfd54cad41ff4928e244db59d678ef0afe7e
Karrie Garzelloni <kgarzelloni@yahoo.com>
2022-01-27 16:52:29 -0700

    seeded dates

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -78,4 +78,33 @@ router.get('/login', (req, res) => {
     res.render('login');
   });
   
+  Talk.ready.then(function () {
+    var me = new Talk.User({
+      id: '123456',
+      name: 'Alice',
+      email: 'alice@example.com',
+      photoUrl: 'https://demo.talkjs.com/img/alice.jpg',
+      welcomeMessage: 'Hey there! How are you? :-)',
+    });
+    window.talkSession = new Talk.Session({
+      appId: 'taEn5Ook',
+      me: me,
+    });
+    var other = new Talk.User({
+      id: '654321',
+      name: 'Sebastian',
+      email: 'Sebastian@example.com',
+      photoUrl: 'https://demo.talkjs.com/img/sebastian.jpg',
+      welcomeMessage: 'Hey, how can I help?',
+    });
+  
+    var conversation = talkSession.getOrCreateConversation(
+      Talk.oneOnOneId(me, other)
+    );
+    conversation.setParticipant(me);
+    conversation.setParticipant(other);
+  
+    var inbox = talkSession.createInbox({ selected: conversation });
+    inbox.mount(document.getElementById('talkjs-container'));
+  });
   module.exports = router;
