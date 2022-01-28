5effa7188bba0d12f248bd8df2dd582fda5449b3
Karrie Garzelloni <kgarzelloni@yahoo.com>
2022-01-28 13:59:15 -0700

    added message sample code to demonstrate functionality of Talkjs

--- public/js/form.js
+++ public/js/form.js
@@ -1,39 +1,64 @@
-
-
 const formFormHandler = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-const username = document.getElementById("floatingInput").value.trim();
+  const username = document.getElementById("floatingInput").value.trim();
 
-const zipcode = document.getElementById("floatingPassword").value.trim();
+  const zipcode = document.getElementById("floatingPassword").value.trim();
 
-const residentCheckBox = document.getElementById("flexCheckDefault").value.trim();
+  const residentCheckBox = document
+    .getElementById("flexCheckDefault")
+    .value.trim();
 
-const retailerCheckBox = document.getElementById("flexCheckChecked").value.trim();
+  const retailerCheckBox = document
+    .getElementById("flexCheckChecked")
+    .value.trim();
 
-const numOfAvailableTests = document.getElementById("tests-available").value.trim();
+  const numOfAvailableTests = document
+    .getElementById("tests-available")
+    .value.trim();
 
-const numOfTestsNeeded = document.getElementById("tests_seeking").value.trim();
+  const numOfTestsNeeded = document
+    .getElementById("tests_seeking")
+    .value.trim();
 
-const comments = document.getElementById("post_comment").value.trim();
+  const comments = document.getElementById("post_comment").value.trim();
 
-const contactInfo = document.getElementById("contact_me").value.trim();
-if (username && zipcode && residentCheckBox && retailerCheckBox && numOfAvailableTests && numOfAvailableTests && numOfTestsNeeded && comments && contactInfo) {
-    const response = await fetch('/api/users', {
-      method: 'POST',
-      body: JSON.stringify({ username , zipcode, residentCheckBox, retailerCheckBox, numOfAvailableTests, numOfAvailableTests, numOfTestsNeeded, comments, contactInfo}),
-      headers: { 'Content-Type': 'application/json' },
+  const contactInfo = document.getElementById("contact_me").value.trim();
+  if (
+    username &&
+    zipcode &&
+    residentCheckBox &&
+    retailerCheckBox &&
+    numOfAvailableTests &&
+    numOfAvailableTests &&
+    numOfTestsNeeded &&
+    comments &&
+    contactInfo
+  ) {
+    const response = await fetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        zipcode,
+        residentCheckBox,
+        retailerCheckBox,
+        numOfAvailableTests,
+        numOfAvailableTests,
+        numOfTestsNeeded,
+        comments,
+        contactInfo,
+      }),
+      headers: { "Content-Type": "application/json" },
     });
 
     if (response.ok) {
-      document.location.create('/content');
+      document.location.create("/content");
     } else {
       alert(response.statusText);
     }
   }
-
-}
+};
 
 document
-.querySelector('.submit-form')
-.addEventListener('submit', formFormHandler);
+  .querySelector(".submit-form")
+  .addEventListener("submit", formFormHandler);
