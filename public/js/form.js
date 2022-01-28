d5b825ddb4975062165430bc39a7a8a27220a1b8
Cesar Olguin <c3sarin3@gmail.com>
2022-01-28 11:15:44 -0500

    added routes js but didnt have time to complete it tutor didnt have enough time

---
+++ public/js/form.js
@@ -0,0 +1,15 @@
+let residentCheckBox = document.getElementById("flexCheckDefault");
+
+let retailerCheckBox = document.getElementById("flexCheckChecked");
+
+let username = document.getElementById("floatingInput");
+
+let Zipcode = document.getElementById("floatingPassword");
+
+let numOfAvailableTests = document.getElementById("tests-available");
+
+let numOfTestsNeeded = document.getElementById("tests_seeking");
+
+let comments = document.getElementById("post_comment");
+
+let contactInfo = document.getElementById("contact_me");
